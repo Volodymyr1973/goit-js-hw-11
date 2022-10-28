d282601e8222873bff9216cb63b9d6d69906432d
Volodymyr <vlrohmanov@gmail.com>
2022-10-28 23:38:58 +0300

    scroll add

--- src/index.js
+++ src/index.js
@@ -97,6 +97,15 @@ function renderPhoto() {
 
   galleryEl.insertAdjacentHTML('beforeend', marcup);
 
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+
   function onClick(event) {
     event.preventDefault();
   }
